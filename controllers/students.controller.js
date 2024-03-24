ddf8b23c2b4fd82efc4910e88bbdc68022a21b1c
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-03-24 11:53:48 +0100

    changed current day to default value

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -235,7 +235,7 @@ const add_weekly_reports = async function (req, res) {
     const companyId = companyInfo._id;
     const currentDay = new Date(Date.now()).getDay();
 
-    if (currentDay === 0) {
+    if (currentDay === 6) {
       res.status(400).json({
         message:
           "We appreciate your hardwork across the week, but submissions are now closed for this week",
