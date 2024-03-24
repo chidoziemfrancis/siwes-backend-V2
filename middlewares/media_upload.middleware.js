0184fcfe5205ce17d4439f7ffb13b1eb0fc8dd0c
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-03-24 15:11:48 +0100

    cloudinary for form upload

--- middlewares/media_upload.middleware.js
+++ middlewares/media_upload.middleware.js
@@ -1,12 +1,34 @@
 const { response, request } = require("express");
+const cloudinary = require('cloudinary').v2;
+const { Readable } = require("stream");
 
-/**
- * Retrieves file from req.files and stores it
- * @param {request} req
- * @param {response} res
- */
+async function uploadStream(buffer) {
+  return new Promise((res, rej) => {
+    const options = {
+        use_filename: true,
+        resource_type: "auto",
+        folder: 'forms'
+    }
+    const theTransformStream = cloudinary.uploader.upload_stream(
+      options,
+      (err, result) => {
+        if (err) return rej(err);
+        res(result);
+      }
+    );
+    let str = Readable.from(buffer);
+    str.pipe(theTransformStream);
+  });
+}
+
 const processFileUpload = async function (req, res, next) {
   try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
     if (!req.files || Object.keys(req.files).length === 0) {
       res.status(400).json({ message: "Please upload a file" });
       return;
@@ -17,25 +39,16 @@ const processFileUpload = async function (req, res, next) {
       return;
     }
 
-    const uploadPath = "./uploads/forms/" + req.files.form.name.toLowerCase();
+    const buffer = req.files.form.data;
+    const result = await uploadStream(buffer);
 
     // update the req.body
     req.body = {
       ...req.body,
       name: req.files.form.name.toLowerCase(),
-      pathToFile: uploadPath,
+      pathToFile: result.secure_url,
     };
-
-    req.files.form.mv(uploadPath, function (err) {
-      if (err) {
-        res
-          .status(500)
-          .json({ message: "Something went wrong while uploading file" });
-        return;
-      }
-
-      next();
-    });
+    next();
   } catch (error) {
     res
       .status(500)
