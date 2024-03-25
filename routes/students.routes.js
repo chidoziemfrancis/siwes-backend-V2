fe3fe8802fe4d433ba4a37a7ad4bd357c21bd055
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-03-25 22:45:41 +0100

    implemented get weekly reports endpoint for students

--- routes/students.routes.js
+++ routes/students.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const {
   get_details,
+  get_weekly_reports,
   add_work_details,
   add_weekly_reports,
   change_password,
@@ -9,6 +10,8 @@ const {
 const { isStudent } = require("./../middlewares/auth.middleware");
 
 router.get("/getDetails", isStudent, get_details);
+
+router.get("/getWeeklyReports", isStudent, get_weekly_reports);
 
 router.post("/workDetails", isStudent, add_work_details);
 
