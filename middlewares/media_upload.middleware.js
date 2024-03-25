331557356de71dd5cdc7cb31e4d29abe24d2f19c
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-03-25 22:23:35 +0100

    moved cloudinary config to app file

--- middlewares/media_upload.middleware.js
+++ middlewares/media_upload.middleware.js
@@ -2,12 +2,6 @@ const { response, request } = require("express");
 const cloudinary = require('cloudinary').v2;
 const { Readable } = require("stream");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
 async function uploadStream(buffer) {
   return new Promise((res, rej) => {
     const options = {
