3bbdfa01a47d594a4a4f567797300d58a169f0a0
william <emelifonwuw@gmail.com>
2024-10-20 16:13:35 +0100

    fic: edited routes

--- routes/coordinators.routes.js
+++ routes/coordinators.routes.js
@@ -56,7 +56,7 @@ router.get("/getWeeklyReports/:studentCode", isCoordinator, get_weekly_reports);
 
 router.get("/weeklyReportScore", isCoordinator, assign_score_for_student_weekly_report);
 
-router.get("/weeklyReportScore", isCoordinator, download_csv_score_for_student_weekly_report);
+router.get("/weeklyReportScore/csv", isCoordinator, download_csv_score_for_student_weekly_report);
 
 router.get("/forms", isCoordinator, get_forms);
 
