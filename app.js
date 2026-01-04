d4d7da2d1566fc7432cb85cb9e903be4a8b4eab9
dev team <devteam@gmail.com>
2026-01-04 16:39:56 +0100

    feat: add new CORS origin for Babcock SIWES website

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const corsOption = {
       "http://localhost:3001",
       "https://babcock-siwes.com",
       "https://bucc-siwes-website.vercel.app",
+      "https://www.babcock-siwes.com/",
       "https://siwes-website.vercel.app",
     ];
 
